a9bcc135174869ddf499ac47656177784af2bec5
Adam Clements <adam.clements@gmail.com>
2014-01-21 15:02:58 +0000

    Add clj-refactor and git-gutter

--- personal/preload/adam.el
+++ personal/preload/adam.el
@@ -4,6 +4,22 @@
 (tool-bar-mode -1)
 (scroll-bar-mode -1)
 
+
+;;; Add in extra repositories/packages
+(require 'package)
+(setq package-archives '( ("gnu" . "http://elpa.gnu.org/packages/")
+                          ("melpa" . "http://melpa.milkbox.net/packages/")))
+(package-initialize)
+
+(when (not package-archive-contents) (package-refresh-contents))
+
+(dolist (p '(multiple-cursors
+             clj-refactor
+             align-cljlet
+             git-gutter-fringe))
+  (when (not (package-installed-p p))
+    (package-install p)))
+
 (custom-set-faces
  '(default ((t (:background "black"
                             :foreground "white"
@@ -21,8 +37,10 @@
 ;; Custom key bindings
 (global-set-key (kbd "C->") 'mc/mark-next-like-this)
 (global-set-key (kbd "C-<") 'mc/mark-previous-like-this)
+
 ;;; Make M-SPC multiline by default
 (global-set-key (kbd "M-SPC") (lambda () (interactive) (just-one-space -1)))
+
 ;;; Pressing enter does a reindent too by default
 (global-set-key "\C-m" 'newline-and-indent)
 
@@ -35,9 +53,33 @@
 (setq prelude-flyspell nil)
 (setq prelude-whitespace nil)
 
-
-
 (disable-theme 'zenburn)
+
+;; Git gutter configuration ----------------------------------------------
+(require 'git-gutter)
+(global-git-gutter-mode t)
+(setq git-gutter:modified-sign "~")
+(setq git-gutter:added-sign    "+") 
+(setq git-gutter:deleted-sign  "-")
+
+(set-face-foreground 'git-gutter:modified "purple")
+(set-face-foreground 'git-gutter:added "green")
+(set-face-foreground 'git-gutter:deleted "red")
+
+;; Stage current hunk
+(global-set-key (kbd "C-x v s") 'git-gutter:stage-hunk)
+
+;; Revert current hunk
+(global-set-key (kbd "C-x v r") 'git-gutter:revert-hunk)
+
+;; Clojure refactor and formatting configuration --------------------------
+(require 'clj-refactor)
+(add-hook 'clojure-mode-hook (lambda ()
+                               (clj-refactor-mode 1)
+                               (cljr-add-keybindings-with-prefix "C-c C-m")
+                               (define-key clojure-mode-map
+                                 (kbd "C-q") 'align-cljlet)))
+
 
 (xterm-mouse-mode)
 
